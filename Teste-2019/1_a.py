757b8da826195e5ccea6742766859686c3b90bfe
joaoprp111 <joaoprp111@gmail.com>
2022-06-04 16:39:00 +0100

    teste 2019

---
+++ Teste-2019/1_a.py
@@ -0,0 +1,18 @@
+def max_diff(lista):
+    min_elem = min(lista)
+    max_elem = max(lista)
+
+    return max_elem - min_elem
+
+def count_char_occur(sentence):
+    occurrences = dict()
+
+    for c in sentence:
+        occurrences.setdefault(c,0)
+        occurrences[c] += 1
+
+    return occurrences
+
+if __name__ == "__main__":
+    r = count_char_occur('Teste de SPLN! Estou preparado???!')
+    print(r)
---
+++ Teste-2019/2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from sys import argv 
+import re 
+
+def fix_lines(str):
+    str = re.sub(r'(\w)\n(\w)',r'\1 \2',str)
+    str = re.sub(r'(\w)-\n(\w)',r'\1\2',str)
+    str = re.sub(r'(\w)-\n-(\w)',r'\1-\2',str)
+    return str
+
+if __name__ == '__main__':
+    file = argv[1]
+    with open(file,'r') as f:
+        content = f.read()
+        print(fix_lines(content))
